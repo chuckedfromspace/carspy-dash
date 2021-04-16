943ffde1ddb2ae9b09ac37a2bce0cc4bf7d2d103
chuckedfromspace <43315257+chuckedfromspace@users.noreply.github.com>
2021-04-16 17:18:59 +0200

    Convert to single file app to fix import issue

--- carspy-dash/app.py
+++ carspy-dash/app.py
@@ -4,10 +4,9 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-
-import sys
-sys.path.append(".")
-from plots import synthesize_cars
+import numpy as np
+import plotly.graph_objects as go
+from carspy import CarsSpectrum
 
 ICONS = 'https://use.fontawesome.com/releases/v5.15.3/css/all.css'
 CUSTOM_CSS = "assets/custom.css"
@@ -434,6 +433,53 @@ app.layout = html.Div(
         footer
     ],
 )
+
+'''
+################################## Plots ######################################
+'''
+INIT_COMP = {'N2': 0.79,
+             'Ar': 0.0,
+             'CO2': 0,
+             'CO': 0,
+             'H2': 0,
+             'O2': 0.21,
+             'H2O': 0,
+             'CH4': 0}
+
+
+def synthesize_cars(pressure=1, temperature=1750, pump_lw=1.2,
+                    nu_start=2250, nu_end=2350,
+                    pump_ls='Gaussian', chi_rs='G-matrix',
+                    convol='Kataoka', doppler_effect=True):
+    synth_mode = {'pump_ls': pump_ls,
+                  'chi_rs': chi_rs,
+                  'convol': convol,
+                  'doppler_effect': doppler_effect,
+                  'chem_eq': False}
+
+    nu = np.linspace(nu_start, nu_end, num=10000)
+    cars = CarsSpectrum(pressure=pressure, init_comp=INIT_COMP,
+                        chi_set="SET 1")
+    _, spect = cars.signal_as(temperature=temperature,
+                              nu_s=nu,
+                              synth_mode=synth_mode,
+                              pump_lw=pump_lw)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=nu, y=spect/spect.max(),
+        mode='lines',
+        name="CARS Signal",
+    ))
+
+    fig.update_traces(hoverinfo='skip', selector=dict(type='scatter'))
+    fig.update_layout(height=400,
+                      margin={'l': 20, 'b': 10, 'r': 10, 't': 10},
+                      xaxis_title="Wavenumber [1/cm]",
+                      yaxis_title="Signal [-]")
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
