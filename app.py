ed4ae203093e99e1474309b218f1308362cab000
chuckedfromspace <43315257+chuckedfromspace@users.noreply.github.com>
2021-05-06 11:16:20 +0200

    Fix indentation

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ class CustomDash(dash.Dash):
 ICONS = 'https://use.fontawesome.com/releases/v5.15.3/css/all.css'
 CUSTOM_CSS = "assets/custom.css"
 app = CustomDash(__name__,
-                suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.COSMO, ICONS])
+                 suppress_callback_exceptions=True,
+                 external_stylesheets=[dbc.themes.COSMO, ICONS])
 app.title = 'CARSpy'
 FLUID_STATE = True
